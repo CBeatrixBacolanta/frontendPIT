87ce326ce0e4c127450e827a7fe2f3f6cb2352ac
CBeatrix <bacolanta.celeste173@gmail.com>
2025-04-04 23:05:15 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import TodoList from "./TodoList.jsx";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      <TodoList />
-    </div>
-  );
-}
+const App = () => {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/todos/')
+            .then(response => setTodos(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>My Todo List</h1>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        {todo.title} - {todo.completed ? 'Completed' : 'Pending'}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
